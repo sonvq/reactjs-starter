a546a0e10a4140b37a187f88235bd1feb2bdd2df
Steve <vuquangson1610@gmail.com>
2018-10-25 14:54:04 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -20,26 +20,89 @@ const list = [
     },
 ];
 
+class Developer {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    getName() {
+        return this.firstname + ' ' + this.lastname;
+    }
+}
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list,
+        };
+
+        this.onDismiss = this.onDismiss.bind(this);
+    }
+
+    onDismiss(id) {
+        const updatedList = this.state.list.filter(item => item.objectID !== id);
+        this.setState({
+            list: updatedList
+        });
+    }
+
     render() {
+        const robin = new Developer('Robin', 'Wieruch');
+        console.log(robin.getName());
+
         const helloWorld = 'Hello, welcome to React,';
         let username = 'sonvq';
         return (
             <div className="App">
                 <h2>{helloWorld} {username}!!!</h2>
                 <ul>
-                {list.map(function (item) {
-                    return (
-                        <li>
+                    {list.map(item =>
+                        <li key={item.objectID}>
                             <span>
                                 <a href={item.url}>{item.title}</a>
                             </span>
                             <span>{item.author}</span>
                             <span>{item.num_comments}</span>
                             <span>{item.points}</span>
-                        </li>
-                    );
-                })}
+                            <span>
+                                    <button onClick={() =>
+                                        this.onDismiss(item.objectID)
+                                    }>
+                                        Dismiss
+                                    </button>
+                                </span>
+                        </li>)}
+                </ul>
+                <ul>
+                    {this.state.list.map(function (item) {
+                        return (
+                            <li key={item.objectID}>
+                                <span>
+                                    <a href={item.url}>{item.title}</a>
+                                </span>
+                                <span>{item.author}</span>
+                                <span>{item.num_comments}</span>
+                                <span>{item.points}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                <ul>
+                    {list.map(item => {
+                        return (
+                            <li key={item.objectID}>
+                                <span>
+                                    <a href={item.url}>{item.title}</a>
+                                </span>
+                                <span>{item.author}</span>
+                                <span>{item.num_comments}</span>
+                                <span>{item.points}</span>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         );
